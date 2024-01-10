abce18ffbd88a3a5af1a5fa673f58d57112da3ce
Andrii Nikitin <anikitin@suse.de>
2024-01-10 13:40:52 +0100

    t: Add system test to use with zypper

--- t/system/02-install-go-rpm.sh
+++ t/system/02-install-go-rpm.sh
@@ -4,9 +4,12 @@ set -ex
 
 make install
 
+(
+cd /tmp
 /usr/share/sypper/script/sypper download -vvv go
 
 rpm -i cache/packages/*/*/*rpm
+)
 
 go env
 echo success
---
+++ t/system/02-install-go-zypper.sh
@@ -0,0 +1,26 @@
+#!../lib/test-in-container-systemd.sh
+
+set -ex
+
+make install
+
+(
+cd /tmp
+
+/usr/share/sypper/script/sypper download -vvv go
+cp -r cache/packages/* /var/cache/zypp/packages/
+
+zypper -nvvv in go | tee z.log
+
+cnt=$(grep 'In cache' z.log | wc -l)
+
+test $cnt -gt 10;
+
+rc=0
+# check that zypper never printed 'Retrieving', (except for repodata)
+grep -v repodata z.log | grep -i Retrieving || rc=$?
+test $rc -gt 0
+)
+go env
+
+echo success
